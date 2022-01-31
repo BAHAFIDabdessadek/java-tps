62c0f5fdf91eb30099a0371b31121986138dcb1f
BAHAFIDabdessadek <abdessadek.bahafid@gmail.com>
2022-01-31 18:26:57 +0100

    InheritanceType JOINED

--- tp10/src/main/java/cigma/pfe/models/Client.java
+++ tp10/src/main/java/cigma/pfe/models/Client.java
@@ -1,24 +1,17 @@
-package cigma.pfe.models;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+package cigma.pfe.Moderls;
+import lombok.*;
 import javax.persistence.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
 @Entity(name = "Tclients")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id ;
-    @Column
     private String name;
 
     public Client(String name) {
         this.name = name; }
+
     public Client(){}
 }
